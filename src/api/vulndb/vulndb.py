8882336be58951c49574a54cfc0dd3f21e30e28a
HomeGrownCoder <bmanradge@gmail.com>
2022-10-10 13:03:54 -0400

    Several Updates to the Dev Branch. Mostly Focus on converting all keywords to a list, and dealing with API timeouts. Code is still synchronous only for now, that will be updated in later revisions. The only working API in this commit is NIST.

--- src/api/vulndb/vulndb.py
+++ src/api/vulndb/vulndb.py
@@ -14,5 +14,21 @@ class VULNDBAPI(vulnbaseapi):
 
 
     def prepare_request(self, requested_keyword=None):
-        self.get_api_specific_data["data"] = f"search={requested_keyword}"
         self.get_api_specific_data["headers"][0]["X-VulDB-ApiKey"] = self.api_key
+
+        self.get_api_specific_data["data"] = f"search={requested_keyword}"
+
+        # for keyword in requested_keyword:
+        #     temp_dict = {}
+        #     temp_dict["url"] = f'{self.get_api_specific_data["url"]}{keyword}'
+        #     self.format_api_requested_data.append(temp_dict)
+
+    # def prepare_request(self, requested_keyword: list =None):
+    #     #Updates Needed
+    #     # self.get_api_specific_data["url"] = self.get_api_specific_data["url"] + requested_keyword
+    #     self.get_api_specific_data["headers"][0]["apiKey"] = self.api_key
+    #
+    #     for keyword in requested_keyword:
+    #         temp_dict = {}
+    #         temp_dict["url"] = f'{self.get_api_specific_data["url"]}{keyword}'
+    #         self.format_api_requested_data.append(temp_dict)
--- src/util/requests_handler/fetch.py
+++ src/util/requests_handler/fetch.py
@@ -14,7 +14,6 @@ class FETCHREP:
         self.request_session = requests.Session()
 
     def fetch_rep(self,multiple_lookups=None):
-        print("Synchronous Get Requests")
 
         # url = self.vuln_api_data["url"]
         headers = self.vuln_api_data["headers"][0] or None
@@ -23,10 +22,12 @@ class FETCHREP:
         interval = self.vuln_api_data.get("interval") or 10
 
         returned_results=[]
+        current_item = 1
         with self.request_session as s:
             fetch_url = getattr(s, self.vuln_api_data["http_method"].lower())
 
             for url in self.vuln_api_fmt_data :
+                print(f"Synchronous Get Requests {current_item}/{len(self.vuln_api_fmt_data)}")
 
                 if multiple_lookups:
                     time.sleep(interval)
@@ -39,6 +40,8 @@ class FETCHREP:
 
                 #sleep based off of API interval skipping first iteration if only one search ran
                 multiple_lookups = True
+
+                current_item +=1
 
         return returned_results
 
